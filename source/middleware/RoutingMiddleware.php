f34d4004b9c7588223d6a20845cab1c3eca2be61
4uruanna <adrien@villalonga.software>
2025-12-18 11:23:31 +0100

    feat: routing middleware

---
+++ source/middleware/RoutingMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Papimod\Routing\middleware;
+
+use Papi\interface\PapiMiddleware;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\App;
+
+final class RoutingMiddleware implements PapiMiddleware
+{
+    public static function register(App $app, array &$middlewares_map): bool
+    {
+        $app->addRoutingMiddleware();
+        return true;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request);
+    }
+}
